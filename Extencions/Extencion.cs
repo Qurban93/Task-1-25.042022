f3ed4862205d28633d3a038afc7748342e59014b
Hp <Hp@DESKTOP-AE4B724>
2022-04-25 01:39:54 +0400

    Добавьте файлы проекта.

---
+++ Extencions/Extencion.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ClassWork_21._04._2022.Models
+{
+     static class Extencion
+    {
+        public static bool isEven(this int a)
+        {
+            return a % 2 == 0;
+        }
+        public static int[] GetValueIndexes(this int[] Arr,int[] Arresult, int Num)
+        {
+            
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                if (Arr[i] == Num)
+                {
+
+                    int IndexArr = Array.IndexOf(Arr, Num,i);
+                    Array.Resize(ref Arresult, Arresult.Length + 1);
+                    Arresult[Arresult.Length - 1] = IndexArr;
+                  
+                }
+            }
+            return Arresult;
+
+        }
+        public static bool IsContainsDigit(this string Str)
+        {
+            bool check = false; 
+
+            foreach (char item in Str)
+            {
+
+                if (char.IsDigit(item))
+                {
+                    check = true;
+                }
+            }
+                return check;
+        }
+        public static int [] GetValueIndexes(this string Name,  int [] arr, char symbol)
+        {
+            
+            for (int i = 0; i < Name.Length; i++)
+            {
+                if(symbol == i)
+                {
+                    int NameIndex = Name.IndexOf(symbol,i);
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length - 1] = NameIndex;
+                   
+                }
+
+            }
+
+            return arr;
+        } 
+        public static bool IsOdd(this int test)
+        {
+            return test % 2 != 0;
+
+
+           
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ClassWork_21._04._2022.Models;
+using System;
+
+
+namespace ClassWork_21._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = { 4, 5,8 ,7, 4, 6, 7, 4,8 ,4};
+            int[] Arr2 = { };
+
+            Arr.GetValueIndexes(Arr2, 4);
+            
+
+        }
+    }
+}
